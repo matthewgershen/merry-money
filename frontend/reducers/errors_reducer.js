7c0caf68ca64d86166c78a84c177f8fe22b140a3
Matthew Gershen <matthewgershen@gmail.com>
2018-09-11 17:04:56 -0400

    Fix typo

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import sessionErrorsReducer from './session_Errors_reducer';
+import sessionErrorsReducer from './session_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer
