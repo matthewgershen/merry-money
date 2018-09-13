4f4d877221ebbaf95b844cb24fcc6c1a1e95d299
Matthew Gershen <matthewgershen@gmail.com>
2018-09-13 13:31:53 -0400

    Style splash page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import LoginForm from './session/login_form_container';
 import SignupForm from './session/signup_form_container';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from './../util/route_util';
+import Splash from './splash/splash';
 
 const App = () => (
   <div>
-    <ProtectedRoute exact path="/" component={GreetingContainer} />
+    <Route exact path="/" component={GreetingContainer} />
+    <Route exact path="/" component={Splash} />
     <AuthRoute path="/login" component={LoginForm} />
     <AuthRoute path="/signup" component={SignupForm} />
   </div>
