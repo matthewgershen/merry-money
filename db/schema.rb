ff89a9bf42b1fc44796fc8ab65b8931ed4c34b0a
Matthew Gershen <matthewgershen@gmail.com>
2018-09-14 10:47:03 -0400

    Add companies table,model, controller, and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_11_173455) do
+ActiveRecord::Schema.define(version: 2018_09_14_133603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "symbol", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_companies_on_name", unique: true
+    t.index ["symbol"], name: "index_companies_on_symbol", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Company.destroy_all
+
+CSV.foreach("/merry-money/lib/data/company_seed.csv") do |row|
+  Company.create(symbol:row[0],name:row[1])
+end
