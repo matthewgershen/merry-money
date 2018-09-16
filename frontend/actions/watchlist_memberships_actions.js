45989b29a9e37f115ca9ec27d6e6008184705c1a
Matthew Gershen <matthewgershen@gmail.com>
2018-09-16 19:15:47 -0400

    Add actions and reducer for watchlist

---
+++ frontend/actions/watchlist_memberships_actions.js
@@ -0,0 +1,42 @@
+import * as WatchlistApiUtil from './../util/watchlist_api_util';
+
+export const RECEIVE_WATCHLIST = "RECEIVE_WATCHLIST";
+export const RECEIVE_WATCHLIST_MEMBERSHIP = "CREATE_WATCHLIST_MEMBERSHIP";
+export const DELETE_WATCHLIST_MEMBERSHIP = "REMOVE_WATCHLIST_MEMBERSHIP";
+
+
+const receiveWatchlist= (watchlist) => {
+  return {
+    type: RECEIVE_WATCHLIST,
+    watchlist
+  };
+};
+
+const receiveWatchlistMembership = (watchlist_membership) => {
+  return {
+    type: RECEIVE_WATCHLIST_MEMBERSHIP,
+    watchlist_membership
+  };
+};
+
+const deleteWatchlistMembership = (watchlist_membership) => {
+  return {
+    type: DELETE_WATCHLIST_MEMBERSHIP,
+    watchlist_membership
+  };
+};
+
+export const fetchWatchlistMemberships = () => dispatch => (
+  WatchlistApiUtil.fetchWatchlistMemberships().then((watchlist) =>
+  dispatch(receiveWatchlist(watchlist)))
+);
+
+export const createWatchlistMembership = (company_id) => dispatch => (
+  WatchlistApiUtil.createWatchlistMembership(company_id).then((watchlist_membership) =>
+  dispatch(receiveWatchlistMembership(watchlist_membership)))
+);
+
+export const removeWatchlistMembership = (id) => dispatch => (
+  WatchlistApiUtil.removeWatchlistMembership(id).then((watchlist_membership) =>
+  dispatch(deleteWatchlistMembership(watchlist_membership)))
+);
