aa37ea9f4a6c7d2c32b5dcc276fac2fb1f5f09cd
Matthew Gershen <matthewgershen@gmail.com>
2018-09-17 12:18:14 -0400

    Fix high, low, volume not showing up on stock show page bug

--- frontend/components/stocks/info.jsx
+++ frontend/components/stocks/info.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 
 const CompanyInfo = (props) =>{
-
-  const vol = (!props.stockInfo.quote.avgTotalVolume) ? "-" : props.stockInfo.quote.avgTotalVolume.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
-  const highToday = (!props.stockInfo.quote.High) ? "-" : props.stockInfo.quote.High.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
-  const lowToday = (!props.stockInfo.quote.Low) ? "-" : props.stockInfo.quote.Low.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
-  const openPrice = (!props.stockInfo.quote.Open) ? "-" : props.stockInfo.quote.Open.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
+  const vol = (!props.stockInfo.quote.avgTotalVolume) ?  "-" : props.stockInfo.quote.avgTotalVolume.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) 
+  const highToday = (!props.stockInfo.quote.high) ?  "-" : props.stockInfo.quote.high.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) 
+  const lowToday = (!props.stockInfo.quote.low) ?  "-" : props.stockInfo.quote.low.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) 
+  const openPrice = (!props.stockInfo.quote.open) ?  "-" : props.stockInfo.quote.open.toLocaleString('en-US', {style: 'currency', currency: 'USD'}) 
 
 
   return(
