57159fdc611c96a75b7ff808d6c86e18bd174385
Matthew Gershen <matthewgershen@gmail.com>
2018-09-20 10:50:36 -0400

    Add snapshot rake task

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,4 +10,8 @@ class Company < ApplicationRecord
     response.last["close"]
   end
 
+  def get_chart
+    day = Time.now.strftime("%Y%m%d")
+    response = HTTParty.get("https://api.iextrading.com/1.0/stock/#{self.symbol}/chart/date/#{day}")
+  end
 end
