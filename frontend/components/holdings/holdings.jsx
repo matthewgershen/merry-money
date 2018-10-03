2ec2d869517d59b0c3706f01014be3e732b7c3f3
Matthew Gershen <matthewgershen@gmail.com>
2018-10-03 17:11:49 -0400

    Add pie chart without labels

--- frontend/components/holdings/holdings.jsx
+++ frontend/components/holdings/holdings.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPortfolioHoldings } from '../../actions/portfolio_holdings_actions';
+import { PieChart, Pie, Sector, Cell, Tooltip } from 'recharts';
+import { selectAllPortfolioHoldings } from './../../reducers/selectors';
 
 class Holdings extends React.Component{
   constructor(props) {
@@ -14,13 +16,32 @@ class Holdings extends React.Component{
 
 
   render(){
-    return <div>Holdings are going here</div>
+
+    if (Object.keys(this.props.holdings).length === 0) {
+        return <div>hang on</div>
+    }else {
+    const data = []
+    this.props.holdings.forEach((holding)=>{
+      let pie = {}
+      pie["name"] = holding.name
+      pie["value"] = holding.shares * holding.price
+      data.push(pie);
+    });
+    
+    return (
+      <PieChart width={800} height={400}>
+        <Pie data={data} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+        <Tooltip/>
+       </PieChart>
+    );
+    }
   }
 }
 
 const mapStateToProps = (state) => {
+
   return{
-    holdings: state.entities.portfolio_holdings
+    holdings: selectAllPortfolioHoldings(state)
   };
 };
 
